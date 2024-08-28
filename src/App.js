b0edc05d2a4a2eef5930a1b46097c519ae3347af
minhosong88 <hominsong@naver.com>
2024-08-28 11:39:40 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { useEffect, useState } from "react";
+
+const TODOS_KEY = "toDos";
 
 function App() {
+  const [toDO, setToDo] = useState("");
+  const [toDos, setToDos] = useState(() => {
+    const savedToDos = localStorage.getItem(TODOS_KEY);
+    return savedToDos ? JSON.parse(savedToDos) : []
+  });
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDO === "") {
+      return;
+    }
+    setToDos((currentArray) => {
+      const newToDos = [toDO, ...currentArray];
+      localStorage.setItem(TODOS_KEY, JSON.stringify(newToDos));
+      return newToDos;
+    });
+    setToDo("");
+  };
+  useEffect(() => {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos))
+  }, [toDos]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDO}
+          type="text"
+          placeholder="Write your things to do" />
+        <button> Add To-Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item.charAt(0).toUpperCase() + item.slice(1)}</li>
+        ))}
+      </ul>
     </div>
   );
 }
